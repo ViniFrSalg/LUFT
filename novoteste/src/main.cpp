1d1c5f168e074875cdb09e78990828d6a316eaf8
vinicius <vinifrinfo@gmail.com>
2026-06-15 19:40:04 -0300

    Sensor optico funcionando

--- novoteste/src/main.cpp
+++ novoteste/src/main.cpp
@@ -21,8 +21,21 @@ Bsec2 envSensor;
 void checkBsecStatus(Bsec2 bsec);
 void newDataCallback(const bme68xData data, const bsecOutputs outputs, Bsec2 bsec);
 
+
+//parte do sensor optico
+const int measurePin = 5; //Connect dust sensor to Arduino A0 pin MUDAR PINO
+const int ledPower = 6;   //Connect 3 led driver pins of dust sensor to Arduino D2  MUDAR PINO
+int samplingTime = 280; // time required to sample signal coming out   of the sensor
+int deltaTime = 40; 
+int sleepTime = 9680;
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+
+
 void setup() {
   Serial.begin(115200);
+  pinMode(ledPower, OUTPUT);
   delay(500);
 
   Wire.begin(I2C_SDA, I2C_SCL);
@@ -66,12 +79,36 @@ void setup() {
                  + String(envSensor.version.minor) + "."
                  + String(envSensor.version.major_bugfix) + "."
                  + String(envSensor.version.minor_bugfix));
+   
+   
 }
 
 void loop() {
   if (!envSensor.run()) {
     checkBsecStatus(envSensor);
   }
+
+   delay(1000);
+   
+   //parte do sensor optico
+
+   digitalWrite(ledPower,LOW); // power on the LED
+   delayMicroseconds(samplingTime);
+   voMeasured = analogRead(measurePin); // read the dust value
+   delayMicroseconds(deltaTime);
+   digitalWrite(ledPower,HIGH); // turn the LED off
+   delayMicroseconds(sleepTime);
+   // 0 - 5V mapped to 0 - 1023 integer values
+   // recover voltage
+   calcVoltage = voMeasured * (5.0 / 1024.0);
+   // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+   // Chris Nafis (c) 2012
+   dustDensity = 170 * calcVoltage - 0.1;
+   Serial.println("SENSOR POEIRA");
+   Serial.println(dustDensity); // unit: ug/m3
+
+   delay(2000);
+
 }
 
 void newDataCallback(const bme68xData data, const bsecOutputs outputs, Bsec2 bsec) {
